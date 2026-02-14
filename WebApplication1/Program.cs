5f13ac046b884ae1f88d1e40ed9a1812147e79b6
George170205 <jorge.ruiz42@uabc.edu.mx>
2026-02-13 22:28:29 -0800

    Config for render

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -21,10 +21,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
+app.Urls.Add("http://0.0.0.0:10000");
 app.Run();
